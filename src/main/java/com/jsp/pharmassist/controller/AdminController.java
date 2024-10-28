138040c8d84737c1010a9a92508cce86a2871914
BarunabhaBasak <barunabhabasak@gamil.com>
2024-10-28 20:44:53 +0530

    Add Admin controller with REST endpoints for CRUD operations

---
+++ src/main/java/com/jsp/pharmassist/controller/AdminController.java
@@ -0,0 +1,43 @@
+package com.jsp.pharmassist.controller;
+
+import com.jsp.pharmassist.entity.Admin;
+import com.jsp.pharmassist.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/admins")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping
+    public List<Admin> getAllAdmins() {
+        return adminService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Admin> getAdminById(@PathVariable String id) {
+        return adminService.findById(id);
+    }
+
+    @PostMapping
+    public Admin createAdmin(@RequestBody Admin admin) {
+        return adminService.save(admin);
+    }
+
+    @PutMapping("/{id}")
+    public Admin updateAdmin(@PathVariable String id, @RequestBody Admin admin) {
+        admin.setAdminId(id);
+        return adminService.save(admin);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAdmin(@PathVariable String id) {
+        adminService.deleteById(id);
+    }
+}
